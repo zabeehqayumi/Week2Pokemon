b540796b1530f76631a9e4bb979c08b778ec329b
Kevin Yu <kevinyu@kevins-macbook-pro.local>
2018-08-13 13:45:23 -0400

    Initial Commit

---
+++ PokemonApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokemonApp
+//
+//  Created by Kevin Yu on 8/13/18.
+//  Copyright © 2018 Kevin Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
